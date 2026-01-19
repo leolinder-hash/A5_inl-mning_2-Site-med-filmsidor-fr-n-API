bbf0f5726d8de202c3136c32de2ac49ea9bcf498
leolinder-hash <leolinder074@gmail.com>
2026-01-19 16:07:20 +0100

    Set up initial SSR-boilerplate

--- index.js
+++ index.js
@@ -5,8 +5,37 @@ const app = express();
 
 app.use(express.static('src'));
 
+async function renderPage (page) {
+const layout = await fs.readFile('./templates/layout.html', 'utf-8');
+const content = await fs.readFile(`./content/${page}.html`, 'utf-8');
+
+const html = layout.replace('::content::', content);
+return html;
+}
+
 app.get('/', async (req, res) => {
-res.send('Servern kör!');
+const html = await renderPage('home');
+res.send(html);
+});
+
+app.get('/about', async (req, res) => {
+const html = await renderPage('about');
+res.send(html);
+});
+
+app.get('/bistro', async (req, res) => {
+const html = await renderPage('bistro');
+res.send(html);
+});
+
+app.get('/contact', async (req, res) => {
+const html = await renderPage('contact');
+res.send(html);
+});
+
+app.get('/event', async (req, res) => {
+const html = await renderPage('event');
+res.send(html);
 });
 
 app.listen(5080, () => {
