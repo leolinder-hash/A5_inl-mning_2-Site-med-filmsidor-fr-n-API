d521fe7292efed9c4b49b57166754cd07784dcd0
leolinder-hash <leolinder074@gmail.com>
2026-01-25 16:20:15 +0100

    Add integration test for movie-page error 404

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -1,8 +1,8 @@
 import request from 'supertest';
 import app from '../index.js';
 
-describe('Movies pages', ()=>{
-  test('GET /movies/:id shows correct movie title', async ()=>{
+describe('Movies pages', () => {
+  test('GET /movies/:id shows correct movie title', async () => {
     const movieId = '3';
 
     const response = await request(app).get(`/movies/${movieId}`);
@@ -11,3 +11,11 @@ describe('Movies pages', ()=>{
     expect(response.text).toContain("The Shawshank Redemption");
   })
 });
+
+describe('Error page', () => {
+  test('GET /movies/:id shows error 404 for non-existing movie', async () => {
+    const response = await request(app).get('/movies/99999');
+    expect(response.status).toBe(404);
+    expect(response.text).toContain('404');
+  })
+})
